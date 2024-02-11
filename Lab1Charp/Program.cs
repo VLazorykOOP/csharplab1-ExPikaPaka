afc08f09914bee4dfab63a97807a070ee89d2077
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-02-11 19:22:09 +0200

    Added testing for task 1

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using MyMath;
+using System.Globalization; // Used for '.' as delimeter in floating-point operations
+class Program {
+    static void Main(string[] args) {
+        // Testing task 1
+        task1();
+    }
+
+    // Task 1 test (Calculates square by providing perimeter
+    private static void task1() {
+        Console.WriteLine("|===~        Testing task 1.1        ~===|");
+        Console.Write("Enter perimeter to calculate square: ");
+
+        try {
+            string line = Console.ReadLine();
+            if (line != null) {
+                double p = double.Parse(line, CultureInfo.InvariantCulture);
+
+                double square = MyMath.Calculation.square(p);
+                Console.WriteLine("Calculated square: " + square.ToString(".000"));
+            }
+        } catch (Exception ex) {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
